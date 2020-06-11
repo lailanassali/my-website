4e91d78dca7a9a5ff17e50edbec81f3f1fdf1af7
Laila <lailapyt@yahoo.com>
2020-06-11 22:05:30 +0100

    add mouse cursor animation

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+let mouseCursor = document.querySelector('.cursor');
+
+let main = document.querySelectorAll('.main-text h1, .main-text a, .content img:nth-child(2)');
+
+window.addEventListener('mousemove', cursor);
+
+function cursor(e) {
+    mouseCursor.style.top = e.pageY + 'px';
+    mouseCursor.style.left = e.pageX + 'px';
+}
+
+main.forEach(link => {
+      link.addEventListener('mouseleave', () => {
+        mouseCursor.classList.remove('link-grow');
+    })
+    link.addEventListener('mouseover', () => {
+        mouseCursor.classList.add('link-grow');
+    })
+});
